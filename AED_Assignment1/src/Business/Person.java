f31fbbf99b3103c647d1fdf6a5e9b725251a8b10
Charmi Dalal <charmidalal503@gmail.com>
2020-09-29 11:33:09 -0400

    Upload Assignment 1 & modify gitignore

---
+++ AED_Assignment1/src/Business/Person.java
@@ -0,0 +1,234 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author charmidalal
+ */
+public class Person {
+    
+    /* Initialize all person related fields */
+    private String firstName;
+    private String lastName;
+    private String birthDate;
+    private String telNum;
+    private String faxNum;
+    private String emailAddress;
+    private String streetAddrs1;
+    private String streetAddrs2;
+    private String city;
+    private String state;
+    private String zipcode;
+    private String ssnNum;
+    private String medicalRecNo;
+    private String healthPlanNo;
+    private String bankAccNo;
+    private String licenseNo;
+    private String vehicleNo;   
+    private String deviceNo;   
+    private String linkedIn; 
+    private String biometric; 
+    private String imagePath; 
+    private String uniqueIdentityNo; 
+    private String ipAddr;
+    private boolean saveFlag;
+
+    /* Set getter and setters for each and every fields */
+    public boolean isSaveFlag() {
+        return saveFlag;
+    }
+
+    public void setSaveFlag(boolean saveFlag) {
+        this.saveFlag = saveFlag;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getTelNum() {
+        return telNum;
+    }
+
+    public void setTelNum(String telNum) {
+        this.telNum = telNum;
+    }
+
+    public String getFaxNum() {
+        return faxNum;
+    }
+
+    public void setFaxNum(String faxNum) {
+        this.faxNum = faxNum;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getStreetAddrs1() {
+        return streetAddrs1;
+    }
+
+    public void setStreetAddrs1(String streetAddrs1) {
+        this.streetAddrs1 = streetAddrs1;
+    }
+
+    public String getStreetAddrs2() {
+        return streetAddrs2;
+    }
+
+    public void setStreetAddrs2(String streetAddrs2) {
+        this.streetAddrs2 = streetAddrs2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getSsnNum() {
+        return ssnNum;
+    }
+
+    public void setSsnNum(String ssnNum) {
+        this.ssnNum = ssnNum;
+    }
+
+    public String getMedicalRecNo() {
+        return medicalRecNo;
+    }
+
+    public void setMedicalRecNo(String medicalRecNo) {
+        this.medicalRecNo = medicalRecNo;
+    }
+
+    public String getHealthPlanNo() {
+        return healthPlanNo;
+    }
+
+    public void setHealthPlanNo(String healthPlanNo) {
+        this.healthPlanNo = healthPlanNo;
+    }
+
+    public String getBankAccNo() {
+        return bankAccNo;
+    }
+
+    public void setBankAccNo(String bankAccNo) {
+        this.bankAccNo = bankAccNo;
+    }
+
+    public String getLicenseNo() {
+        return licenseNo;
+    }
+
+    public void setLicenseNo(String licenseNo) {
+        this.licenseNo = licenseNo;
+    }
+
+    public String getVehicleNo() {
+        return vehicleNo;
+    }
+
+    public void setVehicleNo(String vehicleNo) {
+        this.vehicleNo = vehicleNo;
+    }
+
+    public String getDeviceNo() {
+        return deviceNo;
+    }
+
+    public void setDeviceNo(String deviceNo) {
+        this.deviceNo = deviceNo;
+    }
+
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+
+    public String getBiometric() {
+        return biometric;
+    }
+
+    public void setBiometric(String biometric) {
+        this.biometric = biometric;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getUniqueIdentityNo() {
+        return uniqueIdentityNo;
+    }
+
+    public void setUniqueIdentityNo(String uniqueIdentityNo) {
+        this.uniqueIdentityNo = uniqueIdentityNo;
+    }
+
+    public String getIpAddr() {
+        return ipAddr;
+    }
+
+    public void setIpAddr(String ipAddr) {
+        this.ipAddr = ipAddr;
+    }
+    
+    
+}
